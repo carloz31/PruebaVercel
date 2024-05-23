5a84832574d3dd08c2bea7ec17cfb5cdeb425cdb
Carlos Sanchez <103630012+carloz31@users.noreply.github.com>
2024-05-23 02:42:17 -0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        GOOGLE_CLIENT_ID:"895643509716-4vk4lq1smdd2plqtvpan6ldg83e867ti.apps.googleusercontent.com",
+        //backend: "https://eunoia.inf.pucp.edu.pe"
+        backend:"http://localhost:8080"
+    }
+};
+
+export default nextConfig;
